5d3b4e545600300bf14a47beacad0e91259f2934
pikiaboy <thomas.pham96@yahoo.com>
2016-11-19 21:15:16 -0800

    Thomas-Push2
    Changed input file to make it easier to input everything in.
    Some things had 1 word, some had 2, some had 3. Changed so everything
    was 1.
    
    Main is now putting everything into an array size of 100 for now. WIll
    change to individual objects once recieved BST and Hash files

--- main.cpp
+++ main.cpp
@@ -1,31 +1,71 @@
 #include <iostream>
 #include <fstream>
-
+#include "Bike.h"
 
 using namespace std; 
 
-void readFile(string);
+void readFile(Bike array[],string);
 
 int main()
 {
+	Bike bikeList[100];
+	ifstream inFile; 
 	const char inputFileName[] = "InputData.txt";
-	readFile(inputFileName);
-	
+	readFile(bikeList, inputFileName);
+
 
 	return 0; 
 }
 
 
 
-void readFile(string inputFileName)
+void readFile(Bike bikeList[] , string inputFileName)
 {
 	fstream inFile;
 
 	string data = "";
+	int data2 = 0; 
+	int i = 0; //postion in list
+	inFile.open(inputFileName);
+
+	if (!inFile) 
+	{
+		cout << "error opening input file" << endl; 
+		exit(111); 
+	}
+
+	getline(inFile, data); //geting rid of first line
 
 	while (!inFile.eof())
 	{
-
-	}
 		
-}
+		inFile >> data; 
+		bikeList[i].setSerialNumber(data);
+		
+		cout << data << " ";
+	
+		inFile >> data; 
+		bikeList[i].setMake(data);
+
+		cout << data << " ";
+
+		inFile >> data; 
+		bikeList[i].setFrameMaterial(data);
+		
+		cout << data << " ";
+
+		inFile >> data2; 
+		bikeList[i].setFrameSize(data2);
+		
+		cout << data2 << " "; 
+
+		inFile >> data; 
+		bikeList[i].setSaddle(data); 
+		
+		cout << data << endl;
+		
+		i++; 
+	}
+	
+	cout << "Compelted File input!" << endl; 
+} 
