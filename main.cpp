c9ad675f757462905b1bdb5242ee5aa7da40f845
pikiaboy <thomas.pham96@yahoo.com>
2016-12-03 22:39:01 -0800

    Changes to Main
    Made the menu. Still unsure about how we want to structure the about
    section. Output file still needs to be done.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ using namespace std;
 void readFile(BinarySearchTree<Bike>*, HashList *bikeHash, string);
 void outputFile(BinarySearchTree<Bike>*, string);
 void remove(BinarySearchTree<Bike>*);
+void about();
+
+void menu(BinarySearchTree<Bike>*, HashList*);
 
 int main()
 {
@@ -23,17 +26,73 @@ int main()
 
 	readFile(bikeST, bikeHash, inputFileName);
 
+	menu(bikeST, bikeHash);
 
 	bikeST->printInorderIndented();
-
-	remove(bikeST);
-
-	bikeST->printInorderIndented();
-
+	
+	bikeHash->hashSearch(471);
+	bikeHash->hashDelete(471);
 	bikeHash->hashSearch(471);
 
 	system("pause");
 	return 0;
+}
+
+
+void menu(BinarySearchTree<Bike> *bikeBST, HashList *bikeHash )
+{
+	string outFile = "output.txt";
+	char choice = ' ';
+	cout << "~^&~!Please enter in a choice~^&~!" << endl; 
+	cout << "P - Print BST as indented list to screen" << endl;
+	cout << "A - About the Devs" << endl;
+	cout << "S - Save BST and Hash to output.txt" << endl;
+	cout << "D - Delete a node from BST and Hash" << endl;
+	cout << "Q - Quit" << endl;
+
+	while (choice != 'Q' || choice != 'q')
+	{
+		cin >> choice; 
+		switch (choice)
+		{
+		case 'P':
+		case 'p':
+			bikeBST->printInorderIndented();
+			break;
+
+		case 'A' :
+		case 'a':
+			//print about the devs. How do you want to structure this?
+			break;
+
+		case 'S':
+		case 's':
+			outputFile(bikeBST, outFile);
+			break;
+
+		case 'D':
+		case 'd':
+			//this should also delete from the hash too correct?
+			remove(bikeBST);
+			break;
+
+		case 'Q':
+		case 'q':
+			exit(111);
+			break;
+
+		default: 
+			cout << "Invalid input" << endl;
+			break;
+		}
+	}
+
+}
+
+void about()
+{
+	// just cout the names and date of making the code? 
+	// how do we want to structure this?
 }
 
 void outputFile(BinarySearchTree<Bike>* bikenarySearchTree, string fileName) {
@@ -42,6 +101,8 @@ void outputFile(BinarySearchTree<Bike>* bikenarySearchTree, string fileName) {
 	cout << "Saved" << endl;
 }
 
+
+//Remove should remove from the Hash too correct?
 void remove(BinarySearchTree<Bike>* bikenarySearchTree) 
 {
 	Bike x;
